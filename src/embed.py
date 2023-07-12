1948dfb597175a82642120c076a6bb3046ef21c5
simugrad <137772620+simugrad@users.noreply.github.com>
2023-07-13 00:53:13 +0300

    smol changes

--- src/embed.py
+++ src/embed.py
@@ -2,14 +2,14 @@ import json
 import requests
 import configparser
 import pandas as pd
-from transformers import pipeline
+import sqlite3
 import argparse
 import logging
 
 
-def setup_logger():
+def setup_logger(logging_level):
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging_level)
 
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
@@ -21,7 +21,7 @@ def setup_logger():
     return logger
 
 
-def run_feature_extraction_pipeline(model_id, hf_token, texts):
+def run_feature_extraction_pipeline(model_id, hf_token, texts, logger):
     api_url = (
         f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
     )
@@ -51,7 +51,7 @@ def run_feature_extraction_pipeline(model_id, hf_token, texts):
         raise
 
 
-def save_embeddings_to_csv(embeddings, output_file):
+def save_embeddings_to_csv(embeddings, output_file, logger):
     try:
         embeddings.to_csv(output_file, index=False)
         logger.info(f"Embeddings saved to {output_file}. Shape of embeddings: {embeddings.shape}")
@@ -59,16 +59,29 @@ def save_embeddings_to_csv(embeddings, output_file):
         logger.error(f"Failed to save embeddings to {output_file}: {str(e)}")
 
 
-def embedding_query(json_file_path, model_id, hf_token, output_file):
+def save_embeddings_to_db(embeddings, db_file, logger):
+    try:
+        conn = sqlite3.connect(db_file)
+        embeddings.to_sql('embeddings', conn, if_exists='replace', index=False)
+        logger.info(f"Embeddings saved to database {db_file}. Shape of embeddings: {embeddings.shape}")
+    except Exception as e:
+        logger.error(f"Failed to save embeddings to database {db_file}: {str(e)}")
+    finally:
+        if conn:
+            conn.close()
+
+
+def embedding_query(json_file_path, model_id, hf_token, output_file, db_file, logger):
     with open(json_file_path, "r") as file:
         texts = json.load(file)
 
     sentences = [entry["sentence"] for entry in texts["sentences"]]
 
     try:
-        output = run_feature_extraction_pipeline(model_id, hf_token, sentences)
+        output = run_feature_extraction_pipeline(model_id, hf_token, sentences, logger)
         embeddings = pd.DataFrame(output)
-        save_embeddings_to_csv(embeddings, output_file)
+        save_embeddings_to_csv(embeddings, output_file, logger)
+        save_embeddings_to_db(embeddings, db_file, logger)
     except Exception as e:
         logger.error(f"Embedding query failed: {str(e)}")
 
@@ -76,6 +89,8 @@ def embedding_query(json_file_path, model_id, hf_token, output_file):
 def parse_arguments():
     parser = argparse.ArgumentParser(description="CLI Application for embedding query.")
     parser.add_argument("config_file", help="Path to the config file")
+    parser.add_argument("-l", "--log", help="Set the logging level", default="INFO",
+                        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
     return parser.parse_args()
 
 
@@ -88,10 +103,12 @@ def main():
     model_id = config.get("Settings", "model_id")
     hf_token = config.get("Settings", "hf_token")
     output_file = config.get("Settings", "output_file")
+    db_file = config.get("Settings", "db_file")
 
-    embedding_query(json_file_path, model_id, hf_token, output_file)
+    logger = setup_logger(args.log.upper())
+    
+    embedding_query(json_file_path, model_id, hf_token, output_file, db_file, logger)
 
 
 if __name__ == "__main__":
-    logger = setup_logger()
     main()
